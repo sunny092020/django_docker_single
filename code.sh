6c855d873eadf8707cba620c9816585fffb49ab1
Vu Hoang Giang <vhg.anrakutei@gmail.com>
2021-04-12 11:32:50 +0700

    fix variable

--- code.sh
+++ code.sh
@@ -9,4 +9,4 @@ docker cp $APP_DIR/$APP_NAME/static/$APP_NAME/. common_nginx_1:/home/app/web/sta
 docker cp $APP_DIR/$APP_NAME/static/admin common_nginx_1:/home/app/web/staticfiles/
 docker cp $APP_DIR/$APP_NAME common_web_1:/home/app/web/
 
-docker exec common_web_1 sh -c 'cd /home/app/web/$APP_NAME && django-admin makemessages -a && django-admin compilemessages'
+docker exec common_web_1 sh -c 'cd /home/app/web/'$APP_NAME' && django-admin makemessages -a && django-admin compilemessages'

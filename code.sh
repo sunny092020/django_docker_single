f0c1db12383a3857dd024a0836c509b6577df3b4
Vu Hoang Giang <vhg.anrakutei@gmail.com>
2021-04-12 14:56:32 +0700

    path

--- code.sh
+++ code.sh
@@ -5,7 +5,7 @@ set -e
 APP_DIR=$1
 APP_NAME=$2
 
-docker cp $APP_DIR/$APP_NAME/static/$APP_NAME/. common_nginx_1:/home/app/web/staticfiles/django_app/
+docker cp $APP_DIR/$APP_NAME/static/$APP_NAME/. common_nginx_1:/home/app/web/staticfiles/resources/
 docker cp $APP_DIR/$APP_NAME/static/admin common_nginx_1:/home/app/web/staticfiles/
 docker cp $APP_DIR/$APP_NAME common_web_1:/home/app/web/
 

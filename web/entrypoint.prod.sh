2abab9de15e41b2537ef10106e9a4f6a4ed75469
huyng.anrakutei <huyng.anrakutei@gmail.com>
2021-04-16 16:50:56 +0700

    add faketime

--- web/entrypoint.prod.sh
+++ web/entrypoint.prod.sh
@@ -3,7 +3,8 @@
 if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
-
+    export LD_PRELOAD='/usr/local/lib/faketime/libfaketime.so.1'
+    export FAKETIME="@2020-09-01 00:00:00"
     while ! nc -z $SQL_HOST $SQL_PORT; do
       sleep 0.1
     done

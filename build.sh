3f653a38d06747669bafb2389e100bc54c4ec709
huyng.anrakutei <huyng.anrakutei@gmail.com>
2021-04-20 14:51:29 +0700

    refactor

--- build.sh
+++ build.sh
@@ -6,19 +6,7 @@ ENV=$2
 cp .dockerignore $APP_DIR
 export MAIN_MODULE=smart_assist_maintenance
 
-if [ "$ENV" = "development" ]
-then
-  cp web/Dockerfile.development $APP_DIR/Dockerfile.prod
-  cp web/entrypoint.development.sh $APP_DIR/entrypoint.prod.sh
-  APP_DIR=$APP_DIR docker-compose -f common/docker-compose.production.yml -f common/docker-compose.development.yml up --build 
-else
-  cp web/Dockerfile.production $APP_DIR/Dockerfile.prod
-  cp web/entrypoint.production.sh $APP_DIR/entrypoint.prod.sh
-  APP_DIR=$APP_DIR docker-compose -f common/docker-compose.production.yml up --build 
-fi
-
-
-
-
-
+cp web/Dockerfile.$ENV $APP_DIR/Dockerfile.prod
+cp web/entrypoint.$ENV.sh $APP_DIR/entrypoint.prod.sh
+APP_DIR=$APP_DIR docker-compose -f common/docker-compose.$ENV.yml up --build 
 

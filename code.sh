56dcf755ec7afd110424949d747cb67bcadd416a
Vu Hoang Giang <vhg.anrakutei@gmail.com>
2021-04-24 14:05:30 +0700

    fix collect statics

--- code.sh
+++ code.sh
@@ -6,4 +6,5 @@ APP_DIR=$1
 APP_NAME=$2
 
 docker cp $APP_DIR/$APP_NAME common_web_1:/home/app/web/
-docker exec common_web_1 sh -c 'cd /home/app/web/'$APP_NAME' && python3 manage.py collectstatic && django-admin makemessages -a && django-admin compilemessages'
+docker exec common_web_1 sh -c 'cd /home/app/web/ && python3 manage.py collectstatic --no-input'
+docker exec common_web_1 sh -c 'cd /home/app/web/'$APP_NAME' && django-admin makemessages -a && django-admin compilemessages'

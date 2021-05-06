0304d22d793226359879fd48311759ef991df770
Vu Hoang Giang <vhg.anrakutei@gmail.com>
2021-05-06 10:05:18 +0700

    add script

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-ENV=$1
-docker-compose -f common/docker-compose.$ENV.yml up
+APP_DIR=$1
+ENV=$2
+
+APP_DIR=$APP_DIR docker-compose --env-file $PWD/common/.env.prod -f common/docker-compose.$ENV.yml up -d

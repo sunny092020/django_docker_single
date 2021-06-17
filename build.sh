f2ffc1ba2bec220ba0f337087b30896f7af1386f
Vu Hoang Giang <vhg.anrakutei@gmail.com>
2021-06-17 16:46:13 +0700

    refactor code

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ ENV=$2
 
 cp .dockerignore $APP_DIR
 
-cp web/Dockerfile.$ENV $APP_DIR/Dockerfile.prod
-cp web/entrypoint.$ENV.sh $APP_DIR/entrypoint.prod.sh
+cp web/Dockerfile.$ENV $APP_DIR/Dockerfile
+cp web/entrypoint.$ENV.sh $APP_DIR/entrypoint.sh
 APP_DIR=$APP_DIR docker-compose --env-file $PWD/common/.env.prod -f common/docker-compose.$ENV.yml build
--- web/entrypoint.development.sh
+++ web/entrypoint.development.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
-    export LD_PRELOAD='/usr/local/lib/faketime/libfaketime.so.1'
-    export FAKETIME="@2020-09-01 00:00:00"
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
-fi
-
+echo "Waiting for postgres..."
+export LD_PRELOAD='/usr/local/lib/faketime/libfaketime.so.1'
+export FAKETIME="@2020-09-01 00:00:00"
 exec "$@"
